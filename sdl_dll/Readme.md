3fa7aa0d0e49784896ac3b1adf1af26476cdad77
Manuel Fischer <58823087+manuel-fischer@users.noreply.github.com>
2021-06-12 15:21:02 +0200

    Platform specific libraries

---
+++ sdl_dll/Readme.md
@@ -0,0 +1,2 @@
+You have to install SDL for this example
+- Linux: run `sudo sh install_sdl.sh`
---
+++ sdl_dll/install_sdl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+apt install libsdl2-dev
+apt install libsdl2-image-dev
+apt install libsdl2-ttf-dev
